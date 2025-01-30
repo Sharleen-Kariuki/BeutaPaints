c3ec6e4495d1fee5f3b0b8f0e604a8d15716218d
Sharleen-Kariuki <sharleenwambui28@gmail.com>
2025-01-30 23:44:54 +0300

    original backend url

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors(
     {
-        origin: "https://deploy-mern-1whq.vercel.app",
+        origin: "https://beuta-paints-backend.vercel.app",
         methods: ["POST", "GET"],
         credentials: true,
     }
